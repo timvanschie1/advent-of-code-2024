a603c08e61e3422424b39d1acc3580d05572494d
timvanschie1 <timvanschie1@gmail.com>
2024-12-14 17:12:11 +0100

    Day 2 part 2

--- day2/index.test.js
+++ day2/index.test.js
@@ -1,9 +1,8 @@
-const {test, expect} = require("@jest/globals");
+const { test, expect } = require('@jest/globals');
 const part1 = require('./part1');
 const part2 = require('./part2');
 
-const testData =
-  `7 6 4 2 1
+const testData = `7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
@@ -11,4 +10,4 @@ const testData =
 1 3 6 7 9`;
 
 test('day 2, part 1', () => expect(part1(testData)).toBe(2));
-test('day 2, part 2', () => expect(part2(testData)).toBe(null));
+test('day 2, part 2', () => expect(part2(testData)).toBe(4));
--- day2/part2.js
+++ day2/part2.js
@@ -1,6 +1,75 @@
 function part2(data) {
-    let answer;
-    return answer;
+  const reportsWithLevels = data
+    .split('\n')
+    .map((report) => report.split(' ').map(Number));
+
+  const safeReports = reportsWithLevels.filter((report, i) => {
+    const variations = [report];
+
+    for (let i = 0; i < report.length; i++) {
+      const variation = [...report];
+      variation.splice(i, 1);
+      variations.push(variation);
+    }
+
+    const correctVariations = variations
+      .filter((report) => {
+        const copyString = report.toString();
+
+        report.sort((a, b) => a - b);
+        const reportStringAsc = report.toString();
+
+        report.sort((a, b) => b - a);
+        const reportStringDesc = report.toString();
+
+        return (
+          copyString === reportStringAsc || copyString === reportStringDesc
+        );
+      })
+      .filter((report) => {
+        let hasCorrectSteps = true;
+
+        report.forEach((level, i) => {
+          if (i === 0) return;
+
+          const step = Math.abs(report[i - 1] - level);
+
+          if (step < 1 || step > 3) {
+            hasCorrectSteps = false;
+          }
+        });
+
+        return hasCorrectSteps;
+      });
+
+    return !!correctVariations.length;
+  });
+
+  return safeReports.length;
+}
+
+function checkStep(level, lastLevel, direction) {
+  let newDirection;
+  if (lastLevel > level) {
+    newDirection = 'down';
+  } else if (lastLevel < level) {
+    newDirection = 'up';
+  } else {
+    return [false, direction];
+  }
+
+  if (!!direction && direction !== newDirection) {
+    return [false, direction];
+  }
+
+  const step = Math.abs(lastLevel - level);
+  const incorrectStep = step < 1 || step > 3;
+
+  if (incorrectStep) {
+    return [false, direction];
+  }
+
+  return [true, newDirection];
 }
 
 module.exports = part2;
